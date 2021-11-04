954225de35dab50001fe706a9adfa70e3235ccfa
w1_liamby <weiliang_bai@foxmail.com>
2021-11-04 16:59:56 +0800

    nop

---
+++ src/1005.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    int year_num;
+    float x, y, area; //area表示坐标(x,y)对应半圆的面积
+    cin >> year_num;
+    for (int i = 0; i < year_num; i++) {
+        cin >> x >> y;
+        area = M_PI * (x * x + y * y) / 2;
+        printf("Property %d: This property will begin eroding in year %d\n", i + 1, int(area / 50) + 1);
+    }
+    cout << "END OF OUTPUT";
+    return 0;
+}
